14129a88dfb9555924fd9431ac23c44cdeb5e654
Sahil Singh <sahilhr24@gmail.com>
2025-06-24 11:43:59 +0530

    v2823

---
+++ demo.sh
@@ -0,0 +1,17 @@
+ Volume in drive D has no label.
+ Volume Serial Number is 3EBC-CC5D
+
+ Directory of D:\1. GIT
+
+24-06-2025  11:26    <DIR>          .
+24-06-2025  11:26    <DIR>          ..
+24-06-2025  11:33    <DIR>          Pipeline-For_Cloud_RUN
+24-06-2025  11:40               377 Pipeline-For_Cloud_RUN.bat
+24-06-2025  11:42    <DIR>          Pipeline-For_GCP_VM
+24-06-2025  11:41               371 Pipeline-For_GCP_VM.bat
+24-06-2025  11:43    <DIR>          Pipeline-For_Private_GKE_Cluster
+24-06-2025  11:43               397 Pipeline-For_Private_GKE_Cluster.bat
+24-06-2025  10:56    <DIR>          Pipeline-For_Public_GKE_Cluster
+24-06-2025  11:43               395 Pipeline-For_Public_GKE_Cluster.bat
+               4 File(s)          1,540 bytes
+               6 Dir(s)  189,882,036,224 bytes free
