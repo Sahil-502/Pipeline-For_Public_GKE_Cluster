dd8e1602a21470c2eb61200dee17be4ed1b018d2
Sahil <sahilhr24@gmail.com>
2025-10-04 20:37:57 +0530

    v003

--- demo.sh
+++ demo.sh
@@ -1,21 +1,7 @@
- Volume in drive D is Data-Drive
- Volume Serial Number is AE50-80E0
-
- Directory of D:\Git-502
-
-10-09-2025  12:28 AM    <DIR>          .
-10-09-2025  12:28 AM    <DIR>          ..
-10-09-2025  12:29 AM    <DIR>          cicd-private-gke
-10-09-2025  12:28 AM               360 cicd-private-gke.bat
-10-09-2025  12:32 AM    <DIR>          cloudrun-gke-bridge
-10-09-2025  12:30 AM               366 cloudrun-gke-bridge.bat
-10-09-2025  12:32 AM    <DIR>          Infra-CLI-Guide
-10-09-2025  12:31 AM               358 Infra-CLI-Guide.bat
-09-09-2025  11:34 PM    <DIR>          Pipeline-For_GCP_VM
-10-09-2025  12:31 AM               366 Pipeline-For_GCP_VM.bat
-09-09-2025  11:12 PM    <DIR>          Pipeline-For_Public_GKE_Cluster
-10-09-2025  12:32 AM               390 Pipeline-For_Public_GKE_Cluster.bat
-02-10-2025  01:55 PM    <DIR>          pvt-repo
-10-09-2025  12:32 AM               344 pvt-repo.bat
-               6 File(s)          2,184 bytes
-               8 Dir(s)  118,975,455,232 bytes free
+Dockerfile
+cloudbuild.yaml
+default.conf
+demo.sh
+deployment.yaml
+index.htm
+index.html
